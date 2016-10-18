d94b050babf1e18dd2aa84e7470e9a77ebf9665d
trinhtran <trinhtv060392@gmail.com>
2016-10-18 17:36:19 +0700

    server

--- my-cfn-server/app.js
+++ my-cfn-server/app.js
@@ -16,6 +16,11 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- my-cfn-server/routes/index.js
+++ my-cfn-server/routes/index.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+var temp = [
+  {
+    name: "a",
+    value: 9,
+    min: 2,
+    max: 10
+  },{
+    name: "b",
+    value: 4,
+    min: 1,
+    max: 9
+  },{
+    name: "c",
+    value: 6,
+    min: 0,
+    max: 10
+  },{
+    name: "d",
+    value: 8,
+    min: 1,
+    max: 10
+  }
+];
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json(temp);
 });
 
 module.exports = router;
